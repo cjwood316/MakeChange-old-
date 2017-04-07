7736533d85018ff5f39413bc421ac925238974a7
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-04-07 15:17:28 -0600

    Created readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+## Jake's MakeChange homework project
--- src/money/ChangeMaker.java
+++ src/money/ChangeMaker.java
@@ -1,9 +1,18 @@
 package money;
 
+import java.util.Scanner;
+
 public class ChangeMaker {
 
+	Scanner kb = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		//need method for change
+		// need method for change
+		// check if amount tendered was lower than item price
+		// create values for change
+		double cash;
+		String answer;
+		
 
 	}
 
