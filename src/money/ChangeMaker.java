da81cfc9ee36249058de91f72e92fd94ac762b47
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-04-09 19:51:01 -0600

    first test of program, found it needs to round to nearest whole number

--- src/money/ChangeMaker.java
+++ src/money/ChangeMaker.java
@@ -11,11 +11,18 @@ public class ChangeMaker {
 		double priceOfProduct;
 		double change;
 		double moneyProvided;
+		//alter variable to double to allow change.
 		
 		System.out.println("Please enter purchase price of product: $");
 		priceOfProduct = kb.nextDouble();
 		System.out.print("Enter the amount given by the customer: $");
 		moneyProvided = kb.nextDouble();
 
+		if (moneyProvided > priceOfProduct) {
+            System.out.println("The change is: " + ((moneyProvided - priceOfProduct)));
+            System.out.println("The customer should be given the change as follows:");
+            change = moneyProvided - priceOfProduct;
+            //Tested at this point, need to round the change to nearest whole number.
+		}
 	}
 }
